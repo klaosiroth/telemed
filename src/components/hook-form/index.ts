163ab30a7be94b147650b67dcf65670b3461d8b6
Natthaphon Wichianphong <znatthaphon.w@pttdigital.com>
2023-11-10 17:56:36 +0700

    feat: add rhf-autocomplete and rhf-select components

--- src/components/hook-form/index.ts
+++ src/components/hook-form/index.ts
@@ -1,3 +1,6 @@
+export * from './rhf-select';
+
 export { default as RHFTextField } from './rhf-text-field';
+export { default as RHFAutocomplete } from './rhf-autocomplete';
 
 export { default } from './form-provider';
