9905cd7fb5b77d6f3fdb4fc4cee208a21e3c3802
Natthaphon Wichianphong <znatthaphon.w@pttdigital.com>
2023-12-04 12:14:13 +0700

    Enable video viewer

--- src/sections/case/case-video-player.tsx
+++ src/sections/case/case-video-player.tsx
@@ -1,8 +1,8 @@
 import styles from './case-mission.module.css';
 
 export default function CaseVideoPlayer() {
-  // const videoUrl =
-  //   'https://isgpopen.ezvizlife.com/v3/openlive/BB0537990_1_1.m3u8?expire=1762614043&id=646485035898208256&c=8a5f635eb0&t=a66b8f5f7e5045239d63b15b362b7559a966de4facb5ac9d8329f5e21756ff39&ev=100';
+  const videoUrl =
+    'https://isgpopen.ezvizlife.com/v3/openlive/BB0537990_1_1.m3u8?expire=1762614043&id=646485035898208256&c=8a5f635eb0&t=a66b8f5f7e5045239d63b15b362b7559a966de4facb5ac9d8329f5e21756ff39&ev=100';
   // const rtmpUrl =
   //   'rtmp://vtmsgpzl.ezvizlife.com:1935/v3/openlive/BB0537990_1_2?expire=1762614043&id=646485036133388288&c=8a5f635eb0&t=e8b1959a31016dd957ddcf40e9233e5eb5c4b4a0e50ead1fe4f655699929643f&ev=100';
 
@@ -10,16 +10,16 @@ export default function CaseVideoPlayer() {
     <>
       {[...Array(4)].map((_, index) => (
         <div key={index} className={styles.video}>
-          RTMP Video Streaming
-          {/* <iframe
+          {/* RTMP Video Streaming */}
+          <iframe
             title="Video Player"
             width="100%"
-            height="auto"
+            height="100%"
             src={`https://isgpopen.ezviz.com/console/h5videoPlayer.html?videoUrl=${encodeURIComponent(
               videoUrl
             )}`}
             allowFullScreen
-          /> */}
+          />
         </div>
       ))}
     </>
