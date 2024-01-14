d646f129ba3c24fbc855357c1479b968d34a17b0
Natthaphon Wichianphong <znatthaphon.w@pttdigital.com>
2024-01-15 02:32:52 +0700

    Added HTTP Live Streaming (HLS)

--- src/sections/case/case-video-player.tsx
+++ src/sections/case/case-video-player.tsx
@@ -1,27 +1,49 @@
+import { useCallback, useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
+import { useParams } from 'src/routes/hook';
+import axiosInstance, { API_ENDPOINTS } from 'src/utils/axios';
 import styles from './case-mission.module.css';
 
-export default function CaseVideoPlayer() {
-  // const videoUrl =
-  //   'https://isgpopen.ezvizlife.com/v3/openlive/BB0537990_1_1.m3u8?expire=1762614043&id=646485035898208256&c=8a5f635eb0&t=a66b8f5f7e5045239d63b15b362b7559a966de4facb5ac9d8329f5e21756ff39&ev=100';
-  // const rtmpUrl =
-  //   'rtmp://vtmsgpzl.ezvizlife.com:1935/v3/openlive/BB0537990_1_2?expire=1762614043&id=646485036133388288&c=8a5f635eb0&t=e8b1959a31016dd957ddcf40e9233e5eb5c4b4a0e50ead1fe4f655699929643f&ev=100';
+type Camera = {
+  ambulanceCameraId: string;
+  deviceUrl: string;
+};
 
-  return (
-    <>
-      {[...Array(4)].map((_, index) => (
-        <div key={index} className={styles.video}>
-          RTMP Video Streaming
-          {/* <iframe
-            title="Video Player"
-            width="100%"
-            height="100%"
-            src={`https://isgpopen.ezviz.com/console/h5videoPlayer.html?videoUrl=${encodeURIComponent(
-              videoUrl
-            )}`}
-            allowFullScreen
-          /> */}
-        </div>
-      ))}
-    </>
-  );
+export default function CaseVideoPlayer() {
+  const { id: caseId } = useParams();
+  const [cameras, setCameras] = useState<Camera[]>([]);
+
+  const fetchData = useCallback(async () => {
+    try {
+      const response = await axiosInstance.get(`${API_ENDPOINTS.cases}/${caseId}`);
+      const ambulanceCameras = response.data.ambulance?.ambulanceCamera || [];
+      setCameras(ambulanceCameras);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }, [caseId]);
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  const renderVideoPlayers = () =>
+    cameras.map((camera) => (
+      <div key={camera.ambulanceCameraId} className={styles.video}>
+        <ReactPlayer
+          // url={camera.deviceUrl} // rtmpStreamURL
+          url="https://isgpopen.ezvizlife.com/v3/openlive/BB0537990_1_2.m3u8?expire=1765456203&id=658405916897087488&c=d8add32561&t=114eebb148ae7e95457c9098d60d66631ab4425c0e68a258560fccf285b201d7&ev=100"
+          playing
+          controls={false}
+          width="100%"
+          height="100%"
+          autoPlay
+          loop
+          muted
+          allowFullScreen
+        />
+      </div>
+    ));
+
+  return <>{renderVideoPlayers()}</>;
 }
